b771c6207f312a3a2b72b68ca6b1daa1b7401515
rohilpinto <rohilcrispinto@gmail.com>
2021-06-08 18:20:52 +0530

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
- 
-import './App.css';
+import "./App.css";
+import React from "react";
+
+const Data = () => {
+  return (
+    <React.Fragment>
+      <div className="jokes-data">
+        <div className="jokes-data-container">
+          <div className="btn-container">
+            <button className="btn">Random</button>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="jokes-container">
+        <h1>Jokes Generator</h1>
+        <Data></Data>
+      </div>
     </div>
   );
 }
