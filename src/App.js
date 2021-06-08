16489b5b77e765a77f3a3fa6d97fd8e807f0121e
rohilpinto <rohilcrispinto@gmail.com>
2021-06-08 19:12:34 +0530

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Data = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  // const [joke, setJoke] = useState();
+
+  const url = "https://official-joke-api.appspot.com/jokes/random";
+
+  const fetchJoke = async () => {
+    const fetchUrl = await fetch(url);
+    const fetchedJson = await fetchUrl.json();
+
+    const { id, type, setup, punchline } = fetchedJson;
+
+    
+  };
+
   return (
     <React.Fragment>
       <div className="jokes-data">
         <div className="jokes-data-container">
           <div className="jokes-content-container">
-            <h4 className="jokes">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam tempore ad pariatur minus obcaecati quibusdam enim ex eius nobis labore!</h4>
+            <h4 className="jokes">{punchline}</h4>
           </div>
           <div className="btn-container">
-            <button className="btn">Random</button>
+            <button className="btn" onClick={() => fetchJoke()}>
+              Random
+            </button>
           </div>
         </div>
       </div>
