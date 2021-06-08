c73a9d5a7ce8f7f09e377e1c3d762fa400f82b8c
rohilpinto <rohilcrispinto@gmail.com>
2021-06-08 18:47:16 +0530

    updated styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ const Data = () => {
     <React.Fragment>
       <div className="jokes-data">
         <div className="jokes-data-container">
+          <div className="jokes-content-container">
+            <h4 className="jokes">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam tempore ad pariatur minus obcaecati quibusdam enim ex eius nobis labore!</h4>
+          </div>
           <div className="btn-container">
             <button className="btn">Random</button>
           </div>
