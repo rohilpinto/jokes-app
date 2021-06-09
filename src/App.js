cae4801c49dfdcdb32a57132332ca728e40d081a
rohilpinto <rohilcrispinto@gmail.com>
2021-06-09 12:01:44 +0530

    added joke functionality

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 
 const Data = () => {
-  const [isClicked, setIsClicked] = useState(false);
-
-  // const [joke, setJoke] = useState();
-
+  const [jokeSetup, setJokeSetup] = useState();
+  const [punchline, setPunchline] = useState();
+  const [show, setShow] = useState(false);
+  const [next, setNext] = useState(false);
   const url = "https://official-joke-api.appspot.com/jokes/random";
 
   const fetchJoke = async () => {
@@ -14,19 +14,35 @@ const Data = () => {
 
     const { id, type, setup, punchline } = fetchedJson;
 
-    
+    setJokeSetup(setup);
+    setPunchline(punchline);
   };
+
+  useEffect(() => {
+    fetchJoke();
+  }, []);
+
+  useEffect(() => {
+    fetchJoke();
+  }, [next]);
 
   return (
     <React.Fragment>
       <div className="jokes-data">
         <div className="jokes-data-container">
           <div className="jokes-content-container">
-            <h4 className="jokes">{punchline}</h4>
+            <h4 className="jokes">{jokeSetup}</h4>
+            <p className="punchline ">{show === true ? punchline : null}</p>
           </div>
           <div className="btn-container">
-            <button className="btn" onClick={() => fetchJoke()}>
-              Random
+            {/* <button className="btn" onClick={() => fetchJoke()}>
+              Setup
+            </button> */}
+            <button className="btn" onClick={() => setShow(!show)}>
+              Punchline
+            </button>
+            <button className="btn" onClick={() => setNext(!next)}>
+              Next Joke
             </button>
           </div>
         </div>
